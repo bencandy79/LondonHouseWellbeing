d5544741e211aa0698b38f7b14d42a8e693ea31e
bencandy79 <40864653+bencandy79@users.noreply.github.com>
2018-07-31 20:17:33 +0100

    Create I2_SafetyAndSecurity.py

---
+++ I2_SafetyAndSecurity.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 31 18:10:12 2018
+
+@author: ben.candy
+"""
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.decomposition import PCA
+
+CSV_CRIME = 'https://data.london.gov.uk/download/recorded_crime_summary/866c05de-c5cd-454b-8fe5-9e7c77ea2313/MPS%20Ward%20Level%20Crime%20%28most%20recent%2024%20months%29.csv'
+XLSX_TRAFFIC_FATALITIES = 'https://files.datapress.com/london/dataset/road-casualties-severity-borough/road-casualties-severity-lsoa-msoa-ward.xls'
+
+# import crime data
+try:
+    df_crime = pd.read_csv(CSV_CRIME)
+    df_crime['2017'] = df_crime['201701']+df_crime['201702']+df_crime['201703']+df_crime['201704']+df_crime['201705']+df_crime['201706']+df_crime['201707']+df_crime['201708']+df_crime['201709']+df_crime['201710']+df_crime['201711']+df_crime['201712']
+    df_crime_pivot = df_crime.pivot_table(index='WardCode', columns='Major Category', values='2017',aggfunc='sum')
+    crime_columns = ['Burglary','Criminal Damage','Drugs','Robbery','Sexual Offences','Theft and Handling','Violence Against the Person']
+    df_crime_2017 = pd.DataFrame(df_crime_pivot, columns = crime_columns)
+
+    crime_scaler = StandardScaler() 
+    scaled_values = crime_scaler.fit_transform(df_crime_2017) 
+    df_crime_2017.loc[:,:] = scaled_values
+    
+    df_crime_cov = df_crime_2017.cov()
+    df_crime_corr = df_crime_2017.corr()
+    
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(df_crime_2017)
+    df_crime_principal = pd.DataFrame(principalComponents, columns = ['principal component 1','principal component 2'])
+    
+except:
+    print('Cannot open data file - Crime by Ward')
+    
+# import election data
+try:
+    xlsx_traffic_fatalities = pd.ExcelFile(XLSX_TRAFFIC_FATALITIES)
+    df_traffic_f = pd.read_excel(XLSX_TRAFFIC_FATALITIES, 'Ward 2014 only', skiprows=1)
+    traffic_columns = ['Unnamed: 0','Unnamed: 1','Unnamed: 2', '1 Fatal.9','2 Serious.9','3 Slight.9']
+    df_traffic_fatalities = pd.DataFrame(df_traffic_f, columns = traffic_columns)
+    df_traffic_fatalities.rename(columns={'Unnamed: 0':'Ward_Code','Unnamed: 1':'Ward_Name','Unnamed: 2':'Borough', '1 Fatal.9':'Fatal','2 Serious.9':'Serious','3 Slight.9':'Slight'}, inplace=True)
+    df_traffic_fatalities['score']=(df_traffic_fatalities['Fatal']*10)+(df_traffic_fatalities['Serious']*5)+(df_traffic_fatalities['Slight']*1)
+except:
+    print('Cannot open data file - Traffic Fatalities by Ward')
