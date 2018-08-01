00ab7bd3bc282ed8dd22147cbc044f21df7c89b3
bencandy79 <40864653+bencandy79@users.noreply.github.com>
2018-08-01 10:54:41 +0100

    Update I2_SafetyAndSecurity.py

--- I2_SafetyAndSecurity.py
+++ I2_SafetyAndSecurity.py
@@ -12,6 +12,9 @@ from sklearn.decomposition import PCA
 CSV_CRIME = 'https://data.london.gov.uk/download/recorded_crime_summary/866c05de-c5cd-454b-8fe5-9e7c77ea2313/MPS%20Ward%20Level%20Crime%20%28most%20recent%2024%20months%29.csv'
 XLSX_TRAFFIC_FATALITIES = 'https://files.datapress.com/london/dataset/road-casualties-severity-borough/road-casualties-severity-lsoa-msoa-ward.xls'
 
+STscaler = StandardScaler()
+NMscaler = MinMaxScaler()
+
 # import crime data
 try:
     df_crime = pd.read_csv(CSV_CRIME)
@@ -23,8 +26,7 @@ except:
     crime_columns = ['Burglary','Criminal Damage','Drugs','Robbery','Sexual Offences','Theft and Handling','Violence Against the Person']
     df_crime_2017 = pd.DataFrame(df_crime_pivot, columns = crime_columns)
 
-    crime_scaler = StandardScaler() 
-    crime_scaled_values = crime_scaler.fit_transform(df_crime_2017) 
+    crime_scaled_values = STscaler.fit_transform(df_crime_2017) 
     df_crime_2017.loc[:,:] = crime_scaled_values
     
     df_crime_cov = df_crime_2017.cov()
@@ -44,8 +46,7 @@ except:
     traffic_columns = ['Unnamed: 0','1 Fatal.9','2 Serious.9','3 Slight.9']
     df_traffic_fatalities = pd.DataFrame(df_traffic_f, columns = traffic_columns)
     df_traffic_fatalities.rename(columns={'Unnamed: 0':'Ward_Code','1 Fatal.9':'Fatal','2 Serious.9':'Serious','3 Slight.9':'Slight'}, inplace=True)
-    traffic_scaler = StandardScaler()
-    traffic_scaled_values = traffic_scaler.fit_transform(df_traffic_fatalities)
+    traffic_scaled_values = STscaler.fit_transform(df_traffic_fatalities)
     df_traffic_fatalities.loc[:,:] = traffic_scaled_values
     df_traffic_fatalities['traffic_fatality_score'] = (df_traffic_fatalities['Fatal'] + df_traffic_fatalities['Serious'] + df_traffic_fatalities['Slight'])/3
     
