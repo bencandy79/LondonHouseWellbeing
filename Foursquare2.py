5cfbda4da5a6f04605d3d5b399c534d17c021644
bencandy79 <40864653+bencandy79@users.noreply.github.com>
2018-08-23 15:43:46 +0100

    Update Foursquare2.py

--- Foursquare2.py
+++ Foursquare2.py
@@ -5,28 +5,30 @@ from pandas import DataFrame
 import requests
 
 df_Foursquare = pd.DataFrame()
+FrameList = []
 
 for i in range(1,50):
     for j in range(15,750,15):
         client_id = "XO1QQIQ02JE0EKQXZ4HBOYPHW2DSWJH5EIBN2AOZG2NRZVPK"
         client_secret = "GJGSIAZR4WXLYOHW4VP1JHTAMLH23EZXCWZDVPCZTYY2RQ4V"
-        lat = (51.25 + (i/100))
-        long = (-0.5 + (i/1000))
+        lat = (51.5 + (i/100.0))
+        long = (-0.25 + (j/1000.0))
         category_id = '4d4b7104d754a06370d81259' # cultural space
         distance = 500
         requested_keys = ["categories","id","location","name"]
         url = "https://api.foursquare.com/v2/venues/search?ll=%s,%s&intent=browse&radius=%s&categoryId=%s&limit=2&client_id=%s&client_secret=%s&v=%s" % (lat, long, distance, category_id, client_id, client_secret, time.strftime("%Y%m%d"))
         resp = requests.get(url)
         dataResp = resp.json()
-        if dataResp["response"]['venues'] != []: 
+        if dataResp["response"]['venues'] != []:
             data = DataFrame(dataResp["response"]['venues'])[requested_keys]
             df_FoursquareIteration = pd.DataFrame(data)
-            df_FoursquareIteration["categories"] = df_Foursquare["categories"].apply(lambda x: dict(x[0])['name'])
-            df_FoursquareIteration["lat"] = df_Foursquare["location"].apply(lambda x: dict(x)["lat"])
-            df_FoursquareIteration["long"] = df_Foursquare["location"].apply(lambda x: dict(x)["lng"])
-            df_Foursquare = pd.concat([df_Foursquare,df_FoursquareIteration])
-
-df_Foursquare.drop_duplicates(subset=['id'], keep=False)
+            df_FoursquareIteration["categories"] = df_FoursquareIteration["categories"].apply(lambda x: dict(x[0])['name'])
+            df_FoursquareIteration["lat"] = df_FoursquareIteration["location"].apply(lambda x: dict(x)["lat"])
+            df_FoursquareIteration["long"] = df_FoursquareIteration["location"].apply(lambda x: dict(x)["lng"])
+            FrameList.append(df_FoursquareIteration)
+            
+df_Foursquare = pd.concat(FrameList)
+df_Foursquare.drop_duplicates(subset=['id'],keep=False)
 
 def point_in_poly(x,y,poly):
    # check if point is a vertex
